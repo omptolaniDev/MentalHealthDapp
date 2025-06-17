f53e7271f50d743c6500439af822c540a89ed370
omptolaniDev <chriseguaoba@gmail.com>
2025-06-17 23:56:10 +0100

    Rename temp.js to therapist.js with correct case

--- src/models/temp.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const TherapistSchema = new mongoose.Schema({
-  wallet: { type: String, required: true, unique: true },
-  addedAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.models.Therapist || mongoose.model("Therapist", TherapistSchema);
